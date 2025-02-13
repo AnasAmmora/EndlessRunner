08ae07f76da15a5175f309d7e745e4e38ae38323
anas <anasammora11@gmail.com>
2025-02-13 22:58:12 +0300

    CreateProjectStructure

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private float jumpHeight = 10f;
+    [SerializeField] private Transform Foots;
+
+    private float gravity = -50f;
+    private CharacterController characterController;
+    private Vector3 velocity;
+    [SerializeField] private bool isGrounded;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        isGrounded = Physics.CheckSphere(Foots.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore);
+
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0; 
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump")) 
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        characterController.Move(new Vector3(0, 0, runSpeed) * Time.deltaTime);
+
+        // Vertical Velocity
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    
+}
