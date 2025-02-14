7d351ef23b241cc5859e6316d9dd64c5d3b0f0ff
anas <anasammora11@gmail.com>
2025-02-14 23:01:42 +0300

    v4

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } 
+    public bool isPaused { get; private set; }
+    public bool isStarted { get; private set; }
+
+    [SerializeField] GameObject StartGameButton;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    private void Start()
+    {
+        isPaused = false;
+        isStarted = false;
+    }
+
+    public void StartGame()
+    {
+        isStarted = true;
+        StartGameButton.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+
+    }
+
+    public void SetPauseState(bool pauseState)
+    {
+        isPaused = pauseState;
+
+        if (pauseState)
+        {
+            Time.timeScale = 0f; 
+            Debug.Log("Game is paused.");
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            Debug.Log("Game is resumed.");
+        }
+    }
+
+}
--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerCharacterController : MonoBehaviour
@@ -7,28 +9,44 @@ public class PlayerCharacterController : MonoBehaviour
     [SerializeField] private LayerMask groundLayers;
     [SerializeField] private float runSpeed = 8f;
     [SerializeField] private float jumpHeight = 10f;
-    
+    [SerializeField] private float gravity = -55;
 
-    private float gravity = -55;
+
+
     private CharacterController characterController;
+    private Animator animator;
     private Vector3 velocity;
-    [SerializeField] private bool isGrounded;
+    private bool isGrounded;
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        HandleIdleAnimation();
 
+        if (!GameManager.Instance.isStarted || GameManager.Instance.isPaused)
+            return;
+        Debug.Log(GameManager.Instance.isStarted);
 
+        HandleGravity();
+        HandleMovement();
+         
+    }
+
+    private void HandleGravity()
+    {
         isGrounded = Physics.CheckSphere(transform.position, 0.4f, groundLayers, QueryTriggerInteraction.Ignore);
 
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+            HandleJumpAnimation();
+        }
 
         if (isGrounded && velocity.y < 0)
         {
@@ -39,16 +57,19 @@ public class PlayerCharacterController : MonoBehaviour
             velocity.y += gravity * Time.deltaTime;
         }
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
-        {
-            velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
-
-        characterController.Move(new Vector3(0, 0, runSpeed) * Time.deltaTime);
-
-        // Vertical Velocity
         characterController.Move(velocity * Time.deltaTime);
     }
+    private void HandleMovement()
+    {
+        characterController.Move(new Vector3(0, 0, runSpeed) * Time.deltaTime);
+    }
+    private void HandleIdleAnimation()
+    {
+        animator.SetBool("Started", GameManager.Instance.isStarted);
+    }
 
-
+    private void HandleJumpAnimation()
+    {
+        animator.SetTrigger("Jump");
+    }
 }
